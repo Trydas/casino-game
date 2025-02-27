d32660ec9e9b0b91fca6523c9b997f3760abb8f4
CT <w94284u4bk3@gmail.com>
2025-02-27 23:42:31 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>Casino Game</h1>
       <p>歡迎來到 Casino Game，請選擇您的遊戲！</p>
-      <button onClick={() => alert("Play Blackjack!")}>21 點 (Blackjack)</button>
-      <button onClick={() => alert("Play Baccarat")}>老虎機 (Slot Machine)</button>
+      <button onClick={() => alert("Play Blackjack!")}> Blackjack</button>
+      <button onClick={() => alert("Play Baccarat")}> Baccarat</button>
     </div>
   );
 }
