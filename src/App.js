65ae162b16173a01496ea776698bfe81ffb125d8
CT <w94284u4bk3@gmail.com>
2025-02-27 23:13:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return <h1>Hello, TT 寶包!</h1>;
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Casino Game</h1>
+      <p>歡迎來到 Casino Game，請選擇您的遊戲！</p>
+      <button onClick={() => alert("Play Blackjack!")}>21 點 (Blackjack)</button>
+      <button onClick={() => alert("Play Baccarat")}>老虎機 (Slot Machine)</button>
+    </div>
+  );
 }
 
 export default App;
