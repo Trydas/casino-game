219fc7197ab6ddb515e4c232f89e09445f0fa831
CT <w94284u4bk3@gmail.com>
2025-02-28 22:20:21 +0800

    Update Home.js

--- src/games/Home.js
+++ src/games/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   console.log("Home component loaded");
@@ -8,9 +9,9 @@ function Home() {
       <h1>Welcome to Integrated Casino®</h1>
       <h2>Please select a game</h2>
       <ul>
-        <li><a href="/blackjack">Blackjack</a></li>
-        <li><a href="/baccarat">Baccarat</a></li>
-        <li><a href="/roulette">Roulette</a></li>
+        <li><Link to="/blackjack">Blackjack</Link></li>
+        <li><Link to="/baccarat">Baccarat</Link></li>
+        <li><Link to="/roulette">Roulette</Link></li>
       </ul>
     </div>
   );
