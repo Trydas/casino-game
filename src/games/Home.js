b60e745059e85f4f1d71bbf5fdba02106275e757
CT <w94284u4bk3@gmail.com>
2025-02-28 22:40:25 +0800

    Update Home.js

--- src/games/Home.js
+++ src/games/Home.js
@@ -5,10 +5,10 @@ function Home() {
   console.log("Home component loaded");
 
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to Integrated Casino®</h1>
       <h2>Please select a game</h2>
-      <ul>
+      <ul style={{ listStyle: "none", padding: 0 }}>
         <li><Link to="/blackjack">Blackjack</Link></li>
         <li><Link to="/baccarat">Baccarat</Link></li>
         <li><Link to="/roulette">Roulette</Link></li>
