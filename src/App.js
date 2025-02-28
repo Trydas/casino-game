f1592ddad8ad0828608fd29e7562cd9106a06f42
CT <w94284u4bk3@gmail.com>
2025-02-28 22:25:10 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./games/Home"; // 確保路徑正確
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./games/Home";
+import Blackjack from "./games/Blackjack";  // 確保路徑正確
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blackjack" element={<Blackjack />} />
+      </Routes>
+    </Router>
   );
 }
 
